bf6c01feff04042a9aa26b3fa946488f5c02385c
Sven Jansen <sven.jansen@devicetrust.com>
2022-04-21 15:32:05 +0200

    .

--- create-containers.sh
+++ create-containers.sh
@@ -1,8 +1,17 @@
+# Edit max virtual memory for ELK stack (max virtual memory areas vm.max_map_count [65530] is too low, increase to at least [262144])
+sudo sysctl -w vm.max_map_count=262144 
+
 # Create folder structure
-sudo mkdir -p /srv/docker-host
+sudo mkdir -p /srv
+
+# switch to srv directory
+cd /srv
 
 # Clone repository
-sudo git clone https://github.com/jansvensen/vagrant-ubuntu-hocker-host /srv/docker-host
+sudo git clone https://github.com/jansvensen/vagrant-ubuntu-docker-host.git
+
+# switch to vagrant-ubuntu-docker-host directory
+cd /vagrant-ubuntu-docker-host
 
 # Remove existing containers
 docker-compose rm --stop --force
@@ -11,4 +20,4 @@ docker-compose rm --stop --force
 docker pull sebp/elk
 docker pull splunk/splunk
 
-docker-compose -f /srv/guacamole/docker-compose.yml up -d
+docker-compose -f /srv/vagrant-ubuntu-docker-host/docker-compose.yml up -d
